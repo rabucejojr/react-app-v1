4c3a520e73bda2af7183349c8021f475cfe54c90
rabucejo <rogerhapay@gmail.com>
2022-04-22 13:35:20 +0800

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React from 'react'
 import './App.css'
-import Login from './components/Login'
-
-
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import  Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products'
 
 function App() {
   return (
     <>
-    <div className='App'>
-      <Login/>
-    </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/reports' element={<Reports />}></Route>
+          <Route path='/products' element={<Products />}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import App from './App'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(<App/>)
