9563eec3eefc771adda85565d8d028b7afbe0466
rabucejo <rogerhapay@gmail.com>
2022-04-27 14:04:02 +0800

    cleared files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Reports from './pages/Reports'
-import Dashboard from './pages/Dashboard'
-import Calendar from './pages/Calendar'
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' component={Home}></Route>
-          <Route path='/reports' component={Reports}></Route>
-          <Route path='/calendar' component={Calendar}></Route>
-          <Route path='/products' component={Dashboard}></Route>
-        </Routes>
-      </Router>
+    <div>
+      <h1>Roger Abucejo</h1>
+    </div>
     </>
   )
 }
