c8edae01f7da0164a509a2a926be5fc5e3e979d2
rabucejo <rogerhapay@gmail.com>
2022-05-01 14:22:45 +0800

    added a table reference

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import './App.css'
 import Forms from './pages/Forms'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-
-const theme = createTheme({
-  palette: {
-    main: '#ffb74d',
-  },
-})
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Forms/>
-      </ThemeProvider>
+      <Forms />
     </>
   )
 }
--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -1,21 +1,65 @@
 import React from 'react'
 import { TextField, Button } from '@material-ui/core'
 import { Create, Update, Delete } from '@mui/icons-material'
-
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '../components/Table'
+const useStyles = makeStyles({
+  div: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textfield: {
+    marginTop: '5px',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingRight: '1em',
+    justifyContent: 'center',
+  },
+  space: {
+    margin: '10px',
+  },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonSpace: {
+    margin: '10px',
+  },
+  table:{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
 function Forms() {
+  const classes = useStyles()
   return (
-    <div>
-      <TextField variant='outlined' />
-      <TextField variant='outlined' />
-      <Button variant='contained' color='primary' startIcon={<Create />}>
-        Create
-      </Button>
-      <Button variant='contained' color='primary' startIcon={<Update />}>
-        Update
-      </Button>
-      <Button variant='contained' color='primary' startIcon={<Delete />}>
-        Delete
-      </Button>
+    <div className={classes.div}>
+      <div className={classes.textfield}>
+        <TextField variant='outlined' className={classes.space} />
+        <TextField variant='outlined' />
+      </div>
+      <div className={classes.buttons}>
+        <Button variant='contained' color='primary' startIcon={<Create />}>
+          Create
+        </Button>
+        <Button
+          className={classes.buttonSpace}
+          variant='contained'
+          color='primary'
+          startIcon={<Update />}
+        >
+          Update
+        </Button>
+        <Button variant='contained' color='primary' startIcon={<Delete />}>
+          Delete
+        </Button>
+      </div>
+      <div className={classes.table}>
+        <Table />
+      </div>
     </div>
   )
 }
