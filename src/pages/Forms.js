0ddd2e7bd36cebb72d08d9afe6ace301c0de365a
rabucejo <rogerhapay@gmail.com>
2022-05-04 13:04:25 +0800

    styled components

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { FormControl, InputLabel, Input } from '@mui/material'
+import { styled } from '@mui/system'
+
+const Form = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+})
+
+const Inputs = styled('FormControl')({
+  padding: '10px',
+})
 
 function Forms() {
   return (
-    <div>Forms</div>
+    <Form>
+      <Inputs>
+        <InputLabel htmlFor='uname'>Username</InputLabel>
+        <Input id='uname' variant='outlined' />
+      </Inputs>
+      <Inputs>
+        <InputLabel htmlFor='pass'>Password</InputLabel>
+        <Input id='pass' aria-describedby='my-helper-text' />
+      </Inputs>
+    </Form>
   )
 }
 
